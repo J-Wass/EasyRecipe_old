a1f4eec1b3066fba902dbcdb9767b3c4b90a0eb1
Jonah Wasserman <JonahWasserman97@gmail.com>
2017-05-16 17:20:57 -0400

    Validation and 404 page

---
+++ Error_Pages/404.php
@@ -0,0 +1,29 @@
+<?php
+include('../Resources/scripts.php');
+?>
+<html>
+
+<head>
+    <link rel=stylesheet href="../Resources/site.css" type="text/css" />
+</head>
+
+<body>
+    <div class="container" style="background:white">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1 style="text-align:center">Oh uh!</h1>
+                <h2 style="text-align:center">Something went wrong... (HTML Error 404)
+            </div>
+            <div class="col-sm-4 col-sm-offset-3">
+                <img alt="404" style="height:55%;" src="https://img.clipartfest.com/19d113b2e54b4acac96ce5397f000a03_click-here-for-x-large-png-funny-clipart-man-cooking_3527-2793.png"
+                />
+            </div>
+            <div class="clearfix"></div>
+            <p style="text-align:center">The page you are looking for does not exist.</p>
+            <h4 style="text-align:center">If you're lost, try going to the <a href="../ListRecipes.php">Recipe List</a>, <a href="../index.php">Home Page</a>,
+                or check out a <a>Random Recipe!</a></h4>
+        </div>
+    </div>
+</body>
+
+</html>
